bcd3bf34f29c01ec77c16929157157c7d779bb97
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-01-15 22:32:10 -0700

    master

---
+++ Develop/script.js
@@ -0,0 +1,88 @@
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var empty= "";  
+  var uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  var lowercase = "qwertyuioplkjhgfdsamnbvcxz";
+  var number =   "0123456789";
+  var Symbol =    "!@#$%^&*()_"; 
+
+  var plength = document.getElementById("p-length");
+  var uppercase = document.getElementById("P-uppercase");
+  var lowercase =  document.getElementById("p-lowercase");
+  var pNumber = document.getElementById ("p-number"); 
+  var pSymbol = document.getElementById ("p-symbol"); 
+  var submit = document.getElementById ("submit");
+  var password = document.getElementById("password");
+
+
+  passwordText.value = password;
+
+
+
+
+
+
+  
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+addEventListener.getElementById("click", function(){
+  let password = "generatePassword"; function generatePassword
+  for(let i = 0; i <=complexity; i++){
+    password += initalpassword.chartAt(math.floor(Math.random() * value.length-1 )));
+
+  } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+     let initalpassword = empty;
+     (uppercase.checked) ? initalpassword +=  uppercase: "";
+     (lowercase.checked) ? initalpassword +=  lowercase:""; 
+     (pNumber.checked)   ? initalpassword +=  number:"";   
+     (pSymbol.checked)   ? initalpassword +=  Symbol:""; 
+
+      password.value = generatePassword(plength.value,initalpassword)
+
+ });
+
+   (l,initalpassword) {
+      let password = "generatePassword"; function generatePassword
+    for (let i=0; i<plength;i++)
+     temp+=keylist.charAt(math.floor(keylist.length))
+
+        
+       
+  
+    }  
+    
+     return temp;
+
+       
